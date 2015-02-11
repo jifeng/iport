8741fcf1b158258cd63bf04c5cda8ea35d8b17d4
继风 <jifeng.zjd@taobao.com>
2015-02-11 12:03:02 +0800

    add readme

--- example/index.js
+++ example/index.js
@@ -1,23 +1,3 @@
-# iport
-
-![logo](https://raw.github.com/jifeng/iport/master/logo.png)
-
-web服务中ip和port的统一公里工具类
-
-## 取名
-
-iport = ip + port
-
-## 安装
-
-```
-npm install iport
-```
-
-## 用法
-
-```javascript
-
 var http = require('http');
 var iport = require('iport');
 var connect = require('connect');
@@ -38,11 +18,4 @@ app.use('/normal', iport.middleware());
 var server = http.createServer(app);
 
 server.listen(1723);
-
-```
-
-### 讲解
-
-- ip和port的获取规则: http headers -> http -> tcp
-- headers的默认头信息 `x-forwarded-for-port` 和 `x-forwarded-for-port`
 
